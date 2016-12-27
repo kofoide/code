523c5006dadc79665e3d38f8a11d47134b2c1a41
Eric Kofoid <eric@kofoid.net>
2016-12-27 15:03:58 -0600

    MSSQL work

---
+++ MSSQL/Utility/Adhoc/query_LatestDatabaseRestore.sql
@@ -0,0 +1,21 @@
+WITH AllRestores AS
+(
+SELECT
+    D.name AS DatabaseName
+,   D.create_date AS DatabaseCreateDate
+,   D.compatibility_level AS DatabaseCompatibilityLevel
+,   D.collation_name AS DatabaseCollation
+,   R.restore_date AS RestoreCompleteDate
+,   R.restore_type AS RestoreType
+,   RowNum = ROW_NUMBER() OVER (PARTITION BY D.name ORDER BY R.restore_date DESC)
+FROM
+            [master].sys.databases  D
+LEFT JOIN   msdb.dbo.restorehistory R   ON   D.Name = R.destination_database_name
+)
+SELECT
+    DatabaseName
+,   RestoreCompleteDate
+FROM AllRestores
+WHERE
+    RowNum = 1
+AND DatabaseName IN ('ASM-AH', 'DynamicsWarehouse', 'NHACRM', 'NHACRMReporting', 'NHADATAMART')
