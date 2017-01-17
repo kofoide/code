ad4e24c840b934dc8d32e4c5b9bf7bf629cce457
Eric Kofoid <eric@kofoid.net>
2017-01-17 08:23:56 -0600

    TSQL utilities

--- MSSQL/utility/adhoc/query_Dependencies.sql
+++ MSSQL/utility/adhoc/query_Dependencies.sql
@@ -1,20 +1,20 @@
 DECLARE @objectname1 VARCHAR(128) = 'dbo.usp_DimAccount'
 DECLARE @objectname2 VARCHAR(128) = 'usp_DimAccount'
 
+-- What is referencing the object
 SELECT
     referencing_schema_name
 ,   referencing_entity_name
 ,   referencing_id
 ,   referencing_class_desc
 ,   is_caller_dependent  
-FROM sys.dm_sql_referencing_entities ('crm.FilteredOppProd', 'OBJECT')
+FROM sys.dm_sql_referencing_entities (@objectname1, 'OBJECT')
 
 
-
-
-SELECT
+-- What is the object referencing
+SELECT DISTINCT
     o.type_desc
-,    referenced_database_name
+,   ISNULL(referenced_database_name, 'NHAAnalytics') referenced_database_name
 ,   referenced_schema_name
 ,   referenced_entity_name
 ,   referenced_minor_name
@@ -28,8 +28,41 @@ INNER JOIN  sys.objects o ON a.referenced_id = o.[object_id]
 WHERE referenced_minor_id = 0
 
 
-
-
+-- What is the object referencing and subreferences
 SELECT  space(iteration * 4) + TheFullEntityName + ' (' + rtrim(TheType) + ')' AS ThePath
 FROM    dbo.It_Depends(@objectname2, 1)
 ORDER BY ThePath DESC
+
+
+-- What is the object referencing
+SELECT DISTINCT
+    ISNULL(RO.type_desc, 'Unavailable') AS ObjectType
+,   ISNULL(sed.referenced_server_name, 'ASC-PRD-SQL07') AS ServerName
+,   ISNULL(sed.referenced_database_name, 'NHAAnalytics') AS DatabaseName
+,   sed.referenced_schema_name AS SchemaName
+,   sed.referenced_entity_name AS ObjectName
+
+--,   C.[RowCount] AS RowCountToday
+--,   C1.[RowCount] AS RowCountYesterday
+
+--,   OBJECT_NAME(sed.referencing_id) AS referencing_entity_name
+--,   O.type_desc AS referencing_desciption
+--,   COALESCE(COL_NAME(sed.referencing_id, sed.referencing_minor_id), '(n/a)') AS referencing_minor_id
+--,   COALESCE(COL_NAME(sed.referenced_id, sed.referenced_minor_id), '(n/a)') AS referenced_column_name
+--,   sed.is_caller_dependent
+--,   sed.is_ambiguous
+
+FROM 
+            sys.sql_expression_dependencies SED
+LEFT JOIN   sys.objects                     O   ON  SED.referencing_id = O.object_id
+LEFT JOIN   sys.objects                     RO  ON  SED.referenced_id = RO.object_id
+--LEFT JOIN   eric.TableRowCountTrack         C   ON  ISNULL(SED.referenced_database_name, 'NHAAnalytics') = C.DatabaseName
+--                                                AND SED.referenced_schema_name = C.SchemaName
+--                                                AND SED.referenced_entity_name = C.TableName
+--                                                AND C.RunDate = dbo.CastDate(GETDATE())
+--LEFT JOIN   eric.TableRowCountTrack         C1  ON  ISNULL(SED.referenced_database_name, 'NHAAnalytics') = C1.DatabaseName
+--                                                AND SED.referenced_schema_name = C1.SchemaName
+--                                                AND SED.referenced_entity_name = C1.TableName
+--                                                AND C1.RunDate = dbo.CastDate(GETDATE()-1)
+WHERE
+    SED.referencing_id = OBJECT_ID(@objectname1)
--- MSSQL/utility/helperfunctions/dbo.StringClean.sql
+++ MSSQL/utility/helperfunctions/dbo.StringClean.sql
@@ -13,21 +13,21 @@ BEGIN
     --NULL
     SET @MyString = REPLACE(@MyString,CHAR(0),'');
     --Horizontal Tab
-    SET @MyString = REPLACE(@MyString,CHAR(9),'');
+    SET @MyString = REPLACE(@MyString,CHAR(9),' ');
     --Line Feed
-    SET @MyString = REPLACE(@MyString,CHAR(10),'');
+    SET @MyString = REPLACE(@MyString,CHAR(10),' ');
     --Vertical Tab
-    SET @MyString = REPLACE(@MyString,CHAR(11),'');
+    SET @MyString = REPLACE(@MyString,CHAR(11),' ');
     --Form Feed
-    SET @MyString = REPLACE(@MyString,CHAR(12),'');
+    SET @MyString = REPLACE(@MyString,CHAR(12),' ');
     --Carriage Return
-    SET @MyString = REPLACE(@MyString,CHAR(13),'');
+    SET @MyString = REPLACE(@MyString,CHAR(13),' ');
     --Column Break
-    SET @MyString = REPLACE(@MyString,CHAR(14),'');
+    SET @MyString = REPLACE(@MyString,CHAR(14),' ');
     --Non-breaking space
     SET @MyString = REPLACE(@MyString,CHAR(160),'');
     --space
-    SET @MyString = REPLACE(@MyString,' ','');
+    --SET @MyString = REPLACE(@MyString,' ','');
 
 	SET @MyString = LTRIM(RTRIM(@MyString));
 
