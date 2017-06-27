3bdc11e9e7a27b7c731b68f28a3ac2358663d0f0
Eric Kofoid <eric@kofoid.net>
2017-06-27 08:11:27 -0500

    StringSplit fix null issue

--- MSSQL/Utility/helperfunctions/dbo.StringSplit2.sql
+++ MSSQL/Utility/helperfunctions/dbo.StringSplit2.sql
@@ -24,12 +24,12 @@ BEGIN
 	DECLARE @CardinalOrder INT
 	SET @CardinalOrder = 1
 
-    -- Check if the first value is NULL
-    IF LEFT(@sInputList, 1) = @sDelimiter
-    BEGIN
-        INSERT INTO @List SELECT @key, NULL, @CardinalOrder
-        SET @CardinalOrder = @CardinalOrder + 1
-    END
+    ---- Check if the first value is NULL
+    --IF LEFT(@sInputList, 1) = @sDelimiter
+    --BEGIN
+    --    INSERT INTO @List SELECT @key, NULL, @CardinalOrder
+    --    SET @CardinalOrder = @CardinalOrder + 1
+    --END
 
 	WHILE CHARINDEX(@sDelimiter, @sInputList, 0) <> 0
 	BEGIN
@@ -37,11 +37,8 @@ BEGIN
 			@sItem = RTRIM(LTRIM(SUBSTRING(@sInputList, 1, CHARINDEX(@sDelimiter, @sInputList, 0) - 1)))
 		,	@sInputList = RTRIM(LTRIM(SUBSTRING(@sInputList, CHARINDEX(@sDelimiter, @sInputList, 0) + LEN(@sDelimiter), LEN(@sInputList))))
  
-		IF LEN(@sItem) > 0
-		BEGIN
-			INSERT INTO @List SELECT @key, @sItem, @CardinalOrder
-			SET @CardinalOrder = @CardinalOrder + 1
-		END
+		INSERT INTO @List SELECT @key, CASE WHEN LEN(@sItem) = 0 THEN NULL ELSE @sItem END, @CardinalOrder
+		SET @CardinalOrder = @CardinalOrder + 1
 	END
 
 	IF LEN(@sInputList) > 0
